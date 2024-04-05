9d0140072a93cbe8f47358e10b0f0544cea16c9e
joselacruz <joselacruzperez@gmail.com>
2024-04-04 22:28:17 -0400

    environment

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,8 +1,14 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDocs, collection, addDoc,doc, deleteDoc ,updateDoc} from "firebase/firestore";
-import { getFirestore } from "firebase/firestore";
-
+import { initializeApp } from 'firebase/app';
+import {
+  getDocs,
+  collection,
+  addDoc,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,59 +16,57 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDFWBsBcf-hD-gYrk5POk_lKA5gnNYjsIY",
-    authDomain: "salesml-c4e6c.firebaseapp.com",
-    projectId: "salesml-c4e6c",
-    storageBucket: "salesml-c4e6c.appspot.com",
-    messagingSenderId: "877852535107",
-    appId: "1:877852535107:web:91e7ef77cd94f8a485e7f6"
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
 export const saveToFirebase = async (data, key) => {
-    try {
-      const docRef = await addDoc(collection(db, key), data);
-      return docRef.id
-    } catch (error) {
-      console.error("Error saving text to Firestore:", error);
-    }
-  };
-
-  export const updateFirebase = async ({docID, data}) => {
-
-    try{
-      const ref = doc(db, "items", docID);
-      await updateDoc(ref, data);
-    } catch(error){
-      console.error("Error update doc:", error);
-    }
+  try {
+    const docRef = await addDoc(collection(db, key), data);
+    return docRef.id;
+  } catch (error) {
+    console.error('Error saving text to Firestore:', error);
   }
+};
 
-  export const deleteFireBase = async (docID) => {
-    try{
-      await deleteDoc(doc(db, "items", docID));
-    }
-    catch(error) {
-      console.log("error delete doc id",error)
-    }
+export const updateFirebase = async ({ docID, data }) => {
+  try {
+    const ref = doc(db, 'items', docID);
+    await updateDoc(ref, data);
+  } catch (error) {
+    console.error('Error update doc:', error);
   }
+};
 
-  export const readFirebase = async (collectionName) => {
-    try {
-      const userTextsCollection = collection(db, collectionName);
-      const querySnapshot = await getDocs(userTextsCollection);
-  
-      const documentData = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-  
-      return documentData;
-    } catch (error) {
-      console.error("Error reading data from Firestore:", error);
-      return [];
-    }
-  };
+export const deleteFireBase = async (docID) => {
+  try {
+    await deleteDoc(doc(db, 'items', docID));
+  } catch (error) {
+    console.log('error delete doc id', error);
+  }
+};
+
+export const readFirebase = async (collectionName) => {
+  try {
+    const userTextsCollection = collection(db, collectionName);
+    const querySnapshot = await getDocs(userTextsCollection);
+
+    const documentData = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    return documentData;
+  } catch (error) {
+    console.error('Error reading data from Firestore:', error);
+    return [];
+  }
+};
--- src/utils/savedImgToCloud.js
+++ src/utils/savedImgToCloud.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-export async function savedImgToCloud({selectedFiles}) {
-
-  const imgbbApiKey = "1b91f9a3d5903a4a436a45f8552799cd";
+export async function savedImgToCloud({ selectedFiles }) {
+  const imgbbApiKey = import.meta.env.VITE_IMGBB_API_KEY;
   const apiUrl = `https://api.imgbb.com/1/upload?&key=${imgbbApiKey}`;
   const uploadedImageURLs = [];
 
@@ -10,12 +9,12 @@ export async function savedImgToCloud({selectedFiles}) {
     // Crea un array de promesas para cargar todas las imágenes en paralelo
     const uploadPromises = selectedFiles.map(async (file, index) => {
       const formData = new FormData();
-      formData.append("image", file);
+      formData.append('image', file);
 
       try {
         const response = await axios.post(apiUrl, formData, {
           headers: {
-            "Content-Type": "multipart/form-data",
+            'Content-Type': 'multipart/form-data',
           },
         });
 
@@ -23,37 +22,36 @@ export async function savedImgToCloud({selectedFiles}) {
           const imageURL = response.data.data.url;
           uploadedImageURLs[index] = imageURL; // Almacena la URL en el índice correcto
         } else {
-          console.error("Error al cargar una imagen:", response.statusText);
+          console.error('Error al cargar una imagen:', response.statusText);
         }
       } catch (error) {
-        console.error("Error al cargar una imagen:", error);
+        console.error('Error al cargar una imagen:', error);
       }
     });
 
     // Espera a que se completen todas las promesas de carga de imágenes
     await Promise.all(uploadPromises);
   } catch (error) {
-    console.error("Error en la solicitud:", error);
+    console.error('Error en la solicitud:', error);
   } finally {
     return uploadedImageURLs;
   }
 }
 
-
 export const modifyImages = async function (images) {
-    // Mapea las imágenes a un array de promesas
-    const arrayNewUrl = images;
-    const promises = arrayNewUrl.map(async (img, index) => {
-      if (img instanceof File) {
-        // Reemplaza el objeto File con el resultado de savedImgToCloud
-        const response = await savedImgToCloud({ selectedFiles: [img] });
+  // Mapea las imágenes a un array de promesas
+  const arrayNewUrl = images;
+  const promises = arrayNewUrl.map(async (img, index) => {
+    if (img instanceof File) {
+      // Reemplaza el objeto File con el resultado de savedImgToCloud
+      const response = await savedImgToCloud({ selectedFiles: [img] });
 
-        arrayNewUrl[index] = response[0]; // Guarda la URL resultante
-      }
-      return img; // Si no es una instancia de File, mantenlo sin cambios
-    });
+      arrayNewUrl[index] = response[0]; // Guarda la URL resultante
+    }
+    return img; // Si no es una instancia de File, mantenlo sin cambios
+  });
 
-    // Espera a que todas las promesas se resuelvan
-    await Promise.all(promises);
-    return arrayNewUrl;
-  };
+  // Espera a que todas las promesas se resuelvan
+  await Promise.all(promises);
+  return arrayNewUrl;
+};
